aef3dd0778e2d78ee6e7f9feffc04fed63a56c2b
Anna <git@anna.lgbt>
2022-09-07 05:13:50 -0400

    poo

--- client/Ui/MainWindowTabs/Settings.cs
+++ client/Ui/MainWindowTabs/Settings.cs
@@ -27,6 +27,13 @@ internal class Settings : ITab {
         anyChanged |= vfx |= ImGui.Checkbox("Remove glow effect from signs", ref this.Plugin.Config.RemoveGlow);
         anyChanged |= ImGui.Checkbox("Open the viewer automatically when near a sign", ref this.Plugin.Config.AutoViewer);
         anyChanged |= ImGui.Checkbox("Close the viewer automatically when no signs are nearby", ref this.Plugin.Config.AutoViewerClose);
+
+        if (this.Plugin.Config.AutoViewerClose) {
+            ImGui.TreePush();
+            anyChanged |= ImGui.Checkbox("Hide viewer titlebar", ref this.Plugin.Config.HideTitlebar);
+            ImGui.TreePop();
+        }
+
         anyChanged |= ImGui.SliderFloat("Viewer opacity", ref this.Plugin.Config.ViewerOpacity, 0f, 100.0f, $"{this.Plugin.Config.ViewerOpacity:N3}%%");
 
         var glyph = this.Plugin.Config.DefaultGlyph + 1;
